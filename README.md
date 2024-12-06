69a6221a7a76788066bf16f09d6037ede2fd5712
IsmaelTrentin <pandux66@gmail.com>
2024-12-06 15:33:06 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Forza Quattro
+
+Progetto del primo semestre per il corso I1202.
+
+E' richiesta l'implementazione del gioco 'Forza Quattro' utilizzando il linguaggio di programmazione Java.
+
+Responsabili implementazione:
+
+- Ivan Paljevic
+- Ismael Trentin
+
+## Specifiche 
+
+Vedi ![specs](./ForzaQuattro_I1C_SA2024.pdf)
+
---
+++ src/ForzaQuattro.java
@@ -0,0 +1,15 @@
+/**
+ * Progetto del primo semestre per il corso I1202.
+ * E' richiesta l'implementazione del gioco Forza Quattro.
+ *
+ * @author Ivan Paljevic
+ * @author Ismael Trentin
+ * @version 2024.12.06
+ * @see https://www.icorsi.ch/mod/assign/view.php?id=1101697
+ */
+public class ForzaQuattro {
+
+    public static void main(String[] args) {
+        System.out.println("hello forza quattro);
+    }
+}
